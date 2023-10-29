31530821bd01292e9d66765a5d103962af775e46
Đoàn Văn Anh Hiển <21520227@gm.uit.edu.vn>
2023-10-29 17:49:43 +0700

    add footer + not hover product yet

--- views/footer.php
+++ views/footer.php
@@ -1,1 +1,37 @@
+<link rel="stylesheet" type="text/css" href="../../styles/footer.css">
+<footer>
+    <div class="footer-container">
+        <div class="furnio">
+            <p class="title">Furnio.</p>
+            <p class="address">400 University Drive Suite 200 Coral Gables, <br>FL 33134 USA</p>
+        </div>
+        <div class="links">
+            <p>Links</p>
+            <ul class="footer-list">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="help">
+            <p>Help</p>
+            <ul class="footer-list">
+                <li>Payment Options</li>
+                <li>Returns</li>
+                <li>Privacy Policies</li>
+            </ul>
+        </div>
+        <div class="newsletter">
+            <p>Newsletter</p>
+            <input type="email" placeholder="Enter your email address">
+            <button>SUBCRIBE</button>
+        </div>
+    </div>
+    <div class="line"></div>
+    <p class="copyright">2023 furino. All rights reverved</p>
+</footer>
+
 <?php
+
+?>
