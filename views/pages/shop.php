1b8ec12c29ae52351679987c4008ed7ba901e920
Đoàn Văn Anh Hiển <21520227@gm.uit.edu.vn>
2023-10-29 15:06:35 +0700

    header+banner+fuctionbar

--- views/pages/shop.php
+++ views/pages/shop.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/home.css">
+    <link rel="stylesheet" href="../../styles/shop.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet">
     <title>Lab1</title>
@@ -12,8 +12,43 @@
 
 <body>
 <div class="container">
+    <!-- Header -->
     <?php include '../header.php'; ?>
+    <!-- EndOfHeader -->
+    <!-- Banner -->
+    <div class="banner-container">
+            <div class="banner-content">
+            <h1>Shop</h1>
+            <p>Home > Shop</p>
+            </div>
+        </div>
+        <!-- EndofBanner -->
 
+        <!-- FunctionBar -->
+        <div class="function-container">
+            <div class="functionbar-filter">
+            <img src="./assets/imgs/filter.svg" alt="filter" />
+            <p>Filter</p>
+            </div>
+            <div class="functionbar-4dots">
+            <img src="./assets/imgs/4dots.svg" alt="4dots" />
+            </div>
+            <div class="function-viewlist">
+            <img src="./assets/imgs/viewlist.svg" alt="viewlist" />
+            </div>
+            <div class="function-showresult">Showing 1–16 of 32 results</div>
+            <div class="function-showpagenumber">
+            Show
+            <input type="text" placeholder="16" />
+            </div>
+            <div class="function-sortby">
+            Sort by
+            <input type="text" placeholder="Default" />
+            </div>
+        </div>
+        <!-- Content -->
+        <!-- CustomerServiceBanner -->
+        <!-- Footer  -->
 </div>
 </body>
 
