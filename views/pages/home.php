9bdb3fad0fd43330a2f4b676a458d9c2388fa81c
Bao Chau <chauchau0082@gmail.com>
2023-10-29 17:26:47 +0700

    code footer

--- views/pages/home.php
+++ views/pages/home.php
@@ -5,14 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../styles/home.css">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
   <title>Lab1</title>
 </head>
 
 <body>
   <div class="container">
- <?php include '../header.php';?>
+    <?php include '../header.php'; ?>
 
     <!-- Carsouel -->
     <div class="carsouel">
@@ -55,6 +54,8 @@
       </div>
     </div>
     <!-- End of content -->
+    <?php include '../footer.php'; ?>
+
   </div>
 </body>
 
--- views/pages/shop.php
+++ views/pages/shop.php
@@ -5,16 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/home.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <title>Lab1</title>
 </head>
 
 <body>
-<div class="container">
-    <?php include '../header.php'; ?>
+    <div class="container">
+        <?php include '../header.php'; ?>
 
-</div>
+    </div>
 </body>
 
 </html>
