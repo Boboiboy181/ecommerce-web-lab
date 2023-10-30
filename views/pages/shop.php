3840fc675eb9b373a464e3738d6ad422a86a54ed
Đoàn Văn Anh Hiển <21520227@gm.uit.edu.vn>
2023-10-30 18:10:51 +0700

    create service section to reuse

--- views/pages/shop.php
+++ views/pages/shop.php
@@ -248,37 +248,9 @@
     <!-- EndofContent -->
 
     <!-- CustomerServiceBanner -->
-    <div class='servicecontainer'>
-        <div class='service-content'>
-            <img src="../../assets/imgs/Group.svg" alt="Cup" id='cup'>
-            <div class='main'>
-                <h2>High Quality</h2>
-                <h3>crafted from top materials</h3>
-            </div>
-        </div>
-        <div class='service-content'>
-            <img src="../../assets/imgs/guarantee.svg" alt="tick">
-            <div class='main'>
-                <h2>Warranty Protection</h2>
-                <h3>Over 2 years</h3>
-            </div>
-        </div>
-        <div class='service-content'>
-            <img src="../../assets/imgs/Vector.svg" alt="Shipping">
-            <div class='main'>
-                <h2>Free Shipping</h2>
-                <h3>Order over 150 $</h3>
-            </div>
-        </div>
-        <div class='service-content'>
-            <img src="../../assets/imgs/customer-support.svg" alt="support">
-            <div class='main'>
-                <h2>24 / 7 Support</h2>
-                <h3>Dedicated support</h3>
-            </div>
-        </div>
-    </div> 
-    <!-- Footer  -->
+    <?php include '../servicebanner.php'; ?>
+
+    <!-- Footer -->
     <?php include '../footer.php'; ?>
 </div>
 </body>
