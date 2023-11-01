c5909ea2f537688ef6f1ff37a48af1e163ef94d8
Gia Hai <haidao549@gmail.com>
2023-11-01 17:42:18 +0700

    full home design

--- views/pages/home.php
+++ views/pages/home.php
@@ -2,60 +2,300 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../styles/home.css">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
-    rel="stylesheet">
-  <title>Lab1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../../styles/home.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+		rel="stylesheet">
+	<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.2/css/all.css?fbclid=IwAR2Leèv1ZTLJsKEsnl4HGMf5XRZuPqx5yOFnFaOFbVgCiCeU87S0up6ptKU
+">
+	<title>Lab1</title>
 </head>
 
 <body>
-  <div class="container">
- <?php include '../header.php';?>
+	<div class="container">
+		<?php include '../header.php'; ?>
 
-    <!-- Carsouel -->
-    <div class="carsouel">
-      <div class="carsouel-container">
-        <p class="carsouel-sub-heading">New Arrival</p>
-        <h1 class="carsouel-heading">Discover Our New Collection</h1>
-        <p class="carsouel-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus
-          nec ullamcorper mattis.
-        </p>
-        <button>Buy now</button>
-      </div>
-    </div>
-    <!-- End of carsouel -->
+		<!-- Carsouel -->
+		<div class="carsouel">
+			<div class="carsouel-container">
+				<p class="carsouel-sub-heading">New Arrival</p>
+				<h1 class="carsouel-heading">Discover Our New Collection</h1>
+				<p class="carsouel-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+					luctus nec ullamcorper mattis.
+				</p>
+				<button>Buy now</button>
+			</div>
+		</div>
+		<!-- End of carsouel -->
 
-    <!-- Content -->
-    <div class="content">
-      <div class="content-menu">
-        <h2 class="menu-heading">Browse The Range</h2>
-        <p class="menu-sub-heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <div class="menu-list">
-          <div class="menu-item">
-            <div class="menu-item-img">
-              <img src="../../assets/imgs/image 100.png" alt="">
-            </div>
-            <p>Dining</p>
-          </div>
-          <div class="menu-item">
-            <div class="menu-item-img">
-              <img src="../../assets/imgs/image 101.png" alt="">
-            </div>
-            <p>Living</p>
-          </div>
-          <div class="menu-item">
-            <div class="menu-item-img">
-              <img src="../../assets/imgs/image 106.png" alt="">
-            </div>
-            <p>Bedroom</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- End of content -->
-  </div>
+		<!-- Content -->
+		<div class="content">
+			<div class="content-menu">
+				<h2 class="menu-heading">Browse The Range</h2>
+				<p class="menu-sub-heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<div class="menu-list">
+					<div class="menu-item">
+						<div class="menu-item-img">
+							<img src="../../assets/imgs/image 100.png" alt="">
+						</div>
+						<p>Dining</p>
+					</div>
+					<div class="menu-item">
+						<div class="menu-item-img">
+							<img src="../../assets/imgs/image 101.png" alt="">
+						</div>
+						<p>Living</p>
+					</div>
+					<div class="menu-item">
+						<div class="menu-item-img">
+							<img src="../../assets/imgs/image 106.png" alt="">
+						</div>
+						<p>Bedroom</p>
+					</div>
+				</div>
+			</div>
+			<div class="content-product">
+				<h2 class="menu-heading">Our Products</h2>
+				<div class='product-list'>
+					<div class='row'>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 1.svg" alt="item1">
+							<div class='saleoff'>-30%</div>
+							<div class='nametag'>
+								<h2>Syltherine</h2>
+								<p>Stylish Cafe Chair</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 2.500.000</h3>
+								<strike>
+									<p>Rp 3.500.000</p>
+								</strike>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 2.svg" alt="item2">
+							<div class='nametag'>
+								<h2>Leviosa</h2>
+								<p>Stylish Cafe Chair</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 2.500.000</h3>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 3.svg" alt="item3">
+							<div class='saleoff'>-30%</div>
+							<div class='nametag'>
+								<h2>Lolito</h2>
+								<p>Luxury big sofa</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 7.000.000</h3>
+								<strike>
+									<p>Rp 14.000.000</p>
+								</strike>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 4.svg" alt="item4">
+							<div class='new'>New</div>
+							<div class='nametag'>
+								<h2>Respira</h2>
+								<p id='nametaglast'>Outdoor bar table and stool</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 500.000</h3>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class='row'>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 1.svg" alt="item1">
+							<div class='saleoff'>-30%</div>
+							<div class='nametag'>
+								<h2>Syltherine</h2>
+								<p>Stylish Cafe Chair</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 2.500.000</h3>
+								<strike>
+									<p>Rp 3.500.000</p>
+								</strike>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 2.svg" alt="item2">
+							<div class='nametag'>
+								<h2>Leviosa</h2>
+								<p>Stylish Cafe Chair</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 2.500.000</h3>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 3.svg" alt="item3">
+							<div class='saleoff'>-30%</div>
+							<div class='nametag'>
+								<h2>Lolito</h2>
+								<p>Luxury big sofa</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 7.000.000</h3>
+								<strike>
+									<p>Rp 14.000.000</p>
+								</strike>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+						<div class='content-card'>
+							<img src="../../assets/imgs/image 4.svg" alt="item4">
+							<div class='new'>New</div>
+							<div class='nametag'>
+								<h2>Respira</h2>
+								<p id='nametaglast'>Outdoor bar table and stool</p>
+							</div>
+							<div class='pricetag'>
+								<h3>Rp 500.000</h3>
+							</div>
+							<div class="add-to-cart-layer">
+								<button>Add to cart</button>
+								<div class="icons-btn">
+									<span><i class="fa-solid fa-share-nodes"></i>Share</span>
+									<span><i class="fa-solid fa-code-compare"></i>Compare</span>
+									<span><i class="fa-solid fa-thumbs-up"></i>Like</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<button class="content-product-btn">Show more</button>
+			</div>
+			<div class="content-showoff">
+				<div class="showoff-desc">
+					<h2 class="menu-heading">50+ Beautiful rooms
+						inspiration</h2>
+					<p>Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
+					<button class="content-product-btn">Explore more</button>
+				</div>
+				<div class="showoff-main">
+					<img src="../../assets/imgs/show-off-1.png" alt="" class="main-img">
+					<div class="main-desc">
+						<div>
+							<div class="desc-room-name">
+								<span>01</span>
+								<div class="desc-line"></div>
+								<span>Bed Room</span>
+							</div>
+							<p>Inner Peace</p>
+						</div>
+						<button>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+								<path d="M21 12H3M21 12L15 6M21 12L15 18" stroke="white" stroke-width="1.5" stroke-linecap="round"
+									stroke-linejoin="round" />
+							</svg>
+						</button>
+					</div>
+				</div>
+				<div class="showoff-pre">
+					<div class="pre-imgs-container">
+
+						<img src="../../assets/imgs/showoff-2.png" alt="" class="pre-img pre-img-1">
+						<img src="../../assets/imgs/showoff-3.png" alt="" class="pre-img pre-img-2">
+					</div>
+					<div class="dot active"></div>
+					<div class="dot"></div>
+					<div class="dot"></div>
+					<div class="dot"></div>
+				</div>
+			</div>
+			<div class="content-share">
+				<p>Share your setup with</p>
+				<h2 class="menu-heading">#FuniroFurniture</h2>
+				<div class="imgs-container">
+					<div class="left">
+						<div class="row-1">
+							<img src="../../assets/imgs/share/1da1dc5c1c4cd5091a6369784c3b3366.png" alt="" class="share-img img-1-1">
+							<img src="../../assets/imgs/share/row-1-2.png" alt="" class="share-img img-1-2">
+						</div>
+						<div class="row-2">
+							<img src="../../assets/imgs/share/row-2-1.png" alt="" class="share-img img-2-1">
+							<img src="../../assets/imgs/share/row-2-2.png" alt="" class="share-img img-2-2">
+						</div>
+					</div>
+					<img src="../../assets/imgs/share/main.png" alt="" class="share-img img-main">
+					<div class="right">
+						<div class="row-1">
+							<img src="../../assets/imgs/share/row-1-3.png" alt="" class="share-img img-1-3">
+							<img src="../../assets/imgs/share/row-1-4.png" alt="" class="share-img img-1-4">
+						</div>
+						<div class="row-2">
+							<img src="../../assets/imgs/share/row-2-3.png" alt="" class="share-img img-2-3">
+							<img src="../../assets/imgs/share/row-2-4.png" alt="" class="share-img img-2-4">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End of content -->
+		<?php include '../footer.php'; ?>
+	</div>
 </body>
 
 </html>
